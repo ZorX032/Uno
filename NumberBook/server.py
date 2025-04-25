6826478196a06a081c79b09e34f1eeb3b5e6f062
shuri <shuriknich032@gmail.com>
2025-04-25 15:08:20 +0300

    start

---
+++ NumberBook/server.py
@@ -0,0 +1,10 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def serve_web_app():
+    return send_from_directory('.', 'index.html')  # Передаёт ваш HTML-файл
+
+if __name__ == '__main__':
+    app.run()  # Запуск сервера
