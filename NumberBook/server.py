55873bf3310d58fc5d95209ece5386dff42fe8be
shuri <shuriknich032@gmail.com>
2025-04-25 15:46:31 +0300

    start3

--- NumberBook/server.py
+++ NumberBook/server.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, send_from_directory
 
 app = Flask(__name__)
@@ -7,4 +9,5 @@ def serve_web_app():
     return send_from_directory('.', 'index.html')  # Передаёт ваш HTML-файл
 
 if __name__ == '__main__':
-    app.run()  # Запуск сервера f
+    port = int(os.getenv("PORT", 8000))  # Получаем порт из переменной окружения
+    app.run(host="0.0.0.0", port=port)  # Запускаем сервер с правильными параметрами
